ec692e14c4eb9efab408e2c704b0976269b1a945
Varun Venkatesh <97v.varun@gmail.com>
2018-02-05 16:38:02 +0530

    removed get_parent_path

--- util.c
+++ util.c
@@ -14,17 +14,3 @@ int fill_dir_ent(struct dir_block_t *dir_block, char *entry) {
 	strcpy(dir_block->dir_ent[i].name, entry);
 	return 0;
 }
-
-// please free dup_path in the caller to avoid memory leaks
-char *get_parent_path(const char *path) {
-	char *dup_path = strdup(path);
-	for (int i = strlen(dup_path) - 1; i; --i) {
-		if (dup_path[i] != '/') {
-			dup_path[i] = '\0';		
-		} else {
-			dup_path[i] = '\0';
-			break;
-		}	
-	}
-	return dup_path;
-}
--- util.h
+++ util.h
@@ -1,4 +1,3 @@
 #include "common.h"
 
 void fill_dir_ent(struct dir_block_t *dir_block, char *entry);
-char *get_parent_path(const char *path);
