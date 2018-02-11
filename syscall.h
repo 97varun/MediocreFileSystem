252af3e6c8ea31d00543d5d0c2b47b7ced57e3a8
Varun Venkatesh <97v.varun@gmail.com>
2018-02-11 13:41:39 +0530

    added fd table

--- syscall.h
+++ syscall.h
@@ -1,6 +1,11 @@
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
 #include "common.h"
 
 int sys_mkdir(const char *path, mode_t mode);
 int sys_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset);
 int sys_lstat(const char *path, struct stat *stbuf);
 int sys_init();
+
+#endif
